60df7842a644f60e747b659ce95de9659080931e
Cheryl Picado <cherylm28@live.com>
2025-05-29 18:21:32 -0600

    Renombrado paquete base y clase principal a ClinicaEliteDermaApplication

---
+++ src/main/java/com/elitederma/backend/ClinicaEliteDermaApplication.java
@@ -0,0 +1,15 @@
+package com.elitederma.backend;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ClinicaEliteDermaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaEliteDermaApplication.class, args);
+	}
+
+}
